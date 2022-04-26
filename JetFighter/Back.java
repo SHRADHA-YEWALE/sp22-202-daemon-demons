aec23eea49300985ea29aa8159a34892df799287
Puneet Tokhi <puneettokhi25@gmail.com>
2022-04-26 02:38:57 -0700

    Display game settings options and create new game settings screen

---
+++ JetFighter/Back.java
@@ -0,0 +1,20 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Goes back to the MenuScreen
+ */
+public class Back extends GameSettingsOptions
+{
+       public Back(){
+        GreenfootImage backButton =  new GreenfootImage(120,70);
+        Font adjustedFont = new Font(true, false, 45);
+        backButton.setFont(adjustedFont);
+        backButton.setColor(Color.WHITE);
+        backButton.drawString("Back", 0, 50);
+        setImage(backButton);
+    }
+    public void act()
+    {
+    
+    }
+}
