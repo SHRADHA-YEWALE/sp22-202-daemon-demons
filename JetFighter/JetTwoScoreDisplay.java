1b843001857e792195efe47192123b0228a927d0
Shradha Yewale <shradhayewale@Shradhas-MacBook-Pro.local>
2022-04-28 18:51:56 -0700

    Implemented jet2 score display

--- JetFighter/JetTwoScoreDisplay.java
+++ JetFighter/JetTwoScoreDisplay.java
@@ -8,6 +8,11 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class JetTwoScoreDisplay extends Actor
 {
+    private static JetTwoScoreDisplay jetTwoScoreDisplayInstance;
+    GreenfootImage backgroundImage;
+    GreenfootImage jetTwoScoreText;
+    private static GreenfootImage jetTwoImage = new GreenfootImage("plane-white.png");
+
     /**
      * Act - do whatever the JetTwoScoreDisplay wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
@@ -15,5 +20,27 @@ public class JetTwoScoreDisplay extends Actor
     public void act()
     {
         // Add your action code here.
+    }
+    
+    public static ScoreCardDisplay getInstance() {
+        if (jetTwoScoreDisplayInstance == null) {
+            jetTwoImage.scale(30, 30);
+            jetTwoScoreDisplayInstance = new JetTwoScoreDisplay(JetTwoScore.getScoreInstance().getScore(), jetTwoImage);
+        }
+        return jetTwoScoreDisplayInstance;
+    }
+    
+    public JetTwoScoreDisplay(int score, GreenfootImage jetTwoImage) {
+        backgroundImage = getImage();
+        displayScore(score, jetTwoImage);
+    }
+    
+    public void displayScore(int score, GreenfootImage jetTwoImage) {
+        backgroundImage = getImage();
+        jetTwoScoreText = new GreenfootImage("Jet2 Score : " + score, 25, Color.WHITE, Color.BLACK);
+        backgroundImage.drawImage(jetTwoImage, 15, 20);
+        backgroundImage.drawImage(jetTwoScoreText, 50, 70);
+        backgroundImage.scale(150, 75);
+        setImage(backgroundImage);
     }
 }
