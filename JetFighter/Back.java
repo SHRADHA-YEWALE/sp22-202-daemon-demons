58d0235134b70bbe56954b25bcc27f6af131d9d5
Puneet Tokhi <puneettokhi25@gmail.com>
2022-05-01 04:04:45 -0700

    Updated GameSettingsScreen class to display bullet speed

--- JetFighter/Back.java
+++ JetFighter/Back.java
@@ -7,7 +7,7 @@ public class Back extends GameSettingsOptions
 {
        public Back(){
         GreenfootImage backButton =  new GreenfootImage(120,70);
-        Font adjustedFont = new Font(true, false, 45);
+        Font adjustedFont = new Font(true, false, 30);
         backButton.setFont(adjustedFont);
         backButton.setColor(Color.WHITE);
         backButton.drawString("Back", 0, 50);
