5c4315cef787f411d21fe9186663f5d073483565
Puneet Tokhi <puneettokhi25@gmail.com>
2022-05-01 22:40:33 -0700

    Added the GameConfig class

---
+++ JetFighter/GameConfig.java
@@ -0,0 +1,13 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class GameConfig here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class GameConfig extends Actor
+{
+    private static int bulletSpeed = GameSettingsScreen.getBulletSpeed();
+    private static int jetSpeed =  GameSettingsScreen.getJetSpeed();
+}
