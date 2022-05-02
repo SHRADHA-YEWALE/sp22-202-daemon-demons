4c36083cd2386fe0c4d25f15902a344fba2a31cf
Puneet Tokhi <puneettokhi25@gmail.com>
2022-05-01 22:47:15 -0700

    Updated MenuScreen class and modified PlusButton class for GameSettings screen

--- JetFighter/PlusButton.java
+++ JetFighter/PlusButton.java
@@ -12,15 +12,15 @@ public class PlusButton extends GameSettingsOptions
     GreenfootImage plusLabel =  new GreenfootImage(200,200);
     Font adjustedFont = new Font(true, false, 30);
     plusLabel.setFont(adjustedFont);
-    plusLabel.setColor(Color.LIGHT_GRAY);
+    plusLabel.setColor(Color.WHITE);
     plusLabel.drawString("+", 0, 50);
     setImage(plusLabel);
 }
   
     public void act()
     {
-        checkMouse();
-        incrementBulletSpeed();
+        //checkMouse();
+        //incrementBulletSpeed();
     }
 
 }
