5e5d0a07f1a4927f24b8150e29acedae7b864beb
Puneet Tokhi <puneettokhi25@gmail.com>
2022-05-01 19:34:02 -0700

    Added plus button to increment the bullet speed in settings screen

---
+++ JetFighter/PlusButton.java
@@ -0,0 +1,26 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class PlusButton here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class PlusButton extends GameSettingsOptions
+{
+    public PlusButton(){
+    GreenfootImage plusLabel =  new GreenfootImage(200,200);
+    Font adjustedFont = new Font(true, false, 30);
+    plusLabel.setFont(adjustedFont);
+    plusLabel.setColor(Color.LIGHT_GRAY);
+    plusLabel.drawString("+", 0, 50);
+    setImage(plusLabel);
+}
+  
+    public void act()
+    {
+        checkMouse();
+        incrementBulletSpeed();
+    }
+
+}
