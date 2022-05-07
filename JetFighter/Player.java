b7568165ec020a2f85c29e4c72cb7d0540fbd0b1
Puneet Tokhi <puneettokhi25@gmail.com>
2022-05-07 14:06:19 -0700

    Adding the Player class

---
+++ JetFighter/Player.java
@@ -0,0 +1,26 @@
+/**
+ * Write a description of class Player here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Player  
+{
+    private static String player1 = "Player 1";
+    private static String player2 = "Player 2";
+    
+    public static String getPlayer1Name() {
+        return player1;
+    }
+    
+    public static String getPlayer2Name() {
+        return player2;
+    }
+    
+    public static void setPlayer1Name(String name) {
+        Player.player1 = name;
+    }
+    public static void setPlayer2Name(String name) {
+        Player.player2 = name;
+    }
+}
