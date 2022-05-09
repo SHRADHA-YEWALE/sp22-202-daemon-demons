51a237ca0fecd861da4e1c4b1d55e96242ca8884
Puneet Tokhi <puneettokhi25@gmail.com>
2022-05-09 01:09:26 -0700

    Updated Button class' layout

--- JetFighter/Buttons.java
+++ JetFighter/Buttons.java
@@ -13,102 +13,102 @@ public class Buttons
 
     private static final GreenfootSound clickSound = new GreenfootSound("menu_option.wav");
 
-    public Buttons() {
-        optionsImageMap = new HashMap<>();
-        optionsCommandMap = new HashMap<>();
+        public Buttons() {
+            optionsImageMap = new HashMap<>();
+            optionsCommandMap = new HashMap<>();
 
-        optionsImageMap.put("Play", getPlay());
-        optionsImageMap.put("Settings", getSettings());
-        optionsImageMap.put("Back", getBack());
-        optionsImageMap.put("Quit", getQuit());
+            optionsImageMap.put("Play", getPlay());
+            optionsImageMap.put("Settings", getSettings());
+            optionsImageMap.put("Back", getBack());
+            optionsImageMap.put("Quit", getQuit());
 
-        IMenuCommand playCommand = new MenuCommand();
-        playCommand.setReceiver(new IMenuReceiver() {
-                public void doAction() {
-                    clickSound.play();
-                    if (Player.getPlayer1Name().equals("Player 1")) {
-                        String player1 = Greenfoot.ask("Enter Player 1 Name:");
-                        Player.setPlayer1Name(player1);
+            IMenuCommand playCommand = new MenuCommand();
+            playCommand.setReceiver(new IMenuReceiver() {
+                    public void doAction() {
+                        clickSound.play();
+                        if (Player.getPlayer1Name().equals("Player 1")) {
+                            String player1 = Greenfoot.ask("Enter Player 1 Name:");
+                            Player.setPlayer1Name(player1);
                     }
-                    if (Player.getPlayer2Name().equals("Player 2")) {
-                        String player2 = Greenfoot.ask("Enter Player 2 Name:");
-                        Player.setPlayer2Name(player2);
+                        if (Player.getPlayer2Name().equals("Player 2")) {
+                            String player2 = Greenfoot.ask("Enter Player 2 Name:");
+                            Player.setPlayer2Name(player2);
                     }
-                    Greenfoot.setWorld(new GameScreen());    
+                        Greenfoot.setWorld(new GameScreen());    
                 }
             });
-        optionsCommandMap.put("Play", playCommand);
+            optionsCommandMap.put("Play", playCommand);
 
-        IMenuCommand settingsCommand = new MenuCommand();
-        settingsCommand.setReceiver(new IMenuReceiver() {
-                public void doAction() {
-                    clickSound.play();
-                    Greenfoot.setWorld(new GameSettingsScreen());
-                }
+            IMenuCommand settingsCommand = new MenuCommand();
+            settingsCommand.setReceiver(new IMenuReceiver() {
+                    public void doAction() {
+                        clickSound.play();
+                        Greenfoot.setWorld(new GameSettingsScreen());
+                    }
             });
-        optionsCommandMap.put("Settings", settingsCommand);
+            optionsCommandMap.put("Settings", settingsCommand);
 
-        IMenuCommand backCommand = new MenuCommand();
-        backCommand.setReceiver(new IMenuReceiver() {
-                public void doAction() {
-                    clickSound.play();
-                    Greenfoot.setWorld(new MenuScreen());
-                }
+            IMenuCommand backCommand = new MenuCommand();
+            backCommand.setReceiver(new IMenuReceiver() {
+                    public void doAction() {
+                        clickSound.play();
+                        Greenfoot.setWorld(new MenuScreen());
+                    }
             });
-        optionsCommandMap.put("Back", backCommand);
+            optionsCommandMap.put("Back", backCommand);
 
-        IMenuCommand quitCommand = new MenuCommand();
-        quitCommand.setReceiver(new IMenuReceiver() {
-                public void doAction() {
-                    clickSound.play();
-                    System.exit(0);  // quits game       
-                }
+            IMenuCommand quitCommand = new MenuCommand();
+            quitCommand.setReceiver(new IMenuReceiver() {
+                    public void doAction() {
+                        clickSound.play();
+                        System.exit(0);  // quits game       
+                    }
             });
-        optionsCommandMap.put("Quit", quitCommand);
-    }
+            optionsCommandMap.put("Quit", quitCommand);
+        }
 
-    public GreenfootImage getPlay(){
-        GreenfootImage playButton =  new GreenfootImage(100,60);
-        Font adjustedFont = new Font(true, false, 30);
-        playButton.setFont(adjustedFont);
-        playButton.setColor(Color.WHITE);
-        playButton.drawString("Play", 0, 50);
-        return playButton;
-    }
+        public GreenfootImage getPlay(){
+            GreenfootImage playButton =  new GreenfootImage(100,60);
+            Font adjustedFont = new Font(true, false, 30);
+            playButton.setFont(adjustedFont);
+            playButton.setColor(Color.WHITE);
+            playButton.drawString("Play", 0, 50);
+            return playButton;
+        }
 
-    public GreenfootImage getSettings(){
-        GreenfootImage settingsButton =  new GreenfootImage(165,115);
-        Font adjustedFont = new Font(true, false, 30);
-        settingsButton.setFont(adjustedFont);
-        settingsButton.setColor(Color.WHITE);
-        settingsButton.drawString("Settings", 0, 50);
-        return settingsButton;
-    }
+        public GreenfootImage getSettings(){
+            GreenfootImage settingsButton =  new GreenfootImage(165,115);
+            Font adjustedFont = new Font(true, false, 30);
+            settingsButton.setFont(adjustedFont);
+            settingsButton.setColor(Color.WHITE);
+            settingsButton.drawString("Settings", 0, 50);
+            return settingsButton;
+        }
 
-    public GreenfootImage getBack(){
-        GreenfootImage backButton =  new GreenfootImage(120,70);
-        Font adjustedFont = new Font(true, false, 30);
-        backButton.setFont(adjustedFont);
-        backButton.setColor(Color.WHITE);
-        backButton.drawString("Back", 0, 50);
-        return backButton;
-    }
+        public GreenfootImage getBack(){
+            GreenfootImage backButton =  new GreenfootImage(120,70);
+            Font adjustedFont = new Font(true, false, 30);
+            backButton.setFont(adjustedFont);
+            backButton.setColor(Color.WHITE);
+            backButton.drawString("Back", 0, 50);
+            return backButton;
+        }
 
-    public GreenfootImage getQuit(){
-        GreenfootImage quitButton =  new GreenfootImage(100,60);
-        Font adjustedFont = new Font("Marlboro",true, false, 30);
-        quitButton.setFont(adjustedFont);
-        quitButton.setColor(Color.WHITE);
-        quitButton.drawString("Quit", 0, 50);
-        return quitButton;
-    }
+        public GreenfootImage getQuit(){
+            GreenfootImage quitButton =  new GreenfootImage(100,60);
+            Font adjustedFont = new Font("Marlboro",true, false, 30);
+            quitButton.setFont(adjustedFont);
+            quitButton.setColor(Color.WHITE);
+            quitButton.drawString("Quit", 0, 50);
+            return quitButton;
+        }
 
-    public MenuOption getButton(String buttonType) {
-        GreenfootImage image = optionsImageMap.get(buttonType);
-        IMenuCommand cmd = optionsCommandMap.get(buttonType);
+        public MenuOption getButton(String buttonType) {
+            GreenfootImage image = optionsImageMap.get(buttonType);
+            IMenuCommand cmd = optionsCommandMap.get(buttonType);
 
-        MenuOption option = new MenuOption(image);
-        option.setCommand(cmd);
-        return option;
-    }
+            MenuOption option = new MenuOption(image);
+            option.setCommand(cmd);
+            return option;
+        }
 }
