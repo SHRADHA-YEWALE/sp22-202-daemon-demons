72d3ffae6d29a583fedec0cc5aabfaa2282cead6
SHRADHA-YEWALE <shradhayewale@Shradhas-MacBook-Pro.local>
2022-05-13 19:40:58 -0700

    modified jet1 and jet2

--- JetFighter/Jet2.java
+++ JetFighter/Jet2.java
@@ -6,7 +6,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Jet2 extends Jet implements IDetectHitSubject
+public class Jet2 extends Jet
 {
     public Jet2(int mode) {
         super(mode);
